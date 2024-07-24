ee26af5fbcb9c1d4662db72b280888d74c8dda31
Pallavi Patil <pallavi@isgesolutions.com>
2024-07-24 18:15:41 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USER = 'pallavipatil0713@gmail.com'
+EMAIL_PASSWORD = 'wkys nlir xkfn tzcl'
+EMAIL_SENDER = 'pallavipatil0713@gmail.com'
+EMAIL_RECIPIENT = 'pallavippatil2505@gmail.com'
